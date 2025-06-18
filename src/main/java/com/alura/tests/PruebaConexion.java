7dc31f664aa824487749386025c95b4c0b694984
fraja <frajamorze@gmail.com>
2025-06-18 13:51:48 -0600

    Adding java files

--- src/main/java/com/alura/tests/PruebaConexion.java
+++ src/main/java/com/alura/tests/PruebaConexion.java
@@ -9,10 +9,9 @@ public class PruebaConexion {
 	public static void main(String[] args) throws SQLException {
 		Connection con = DriverManager.getConnection(
 				"jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
-				"root", 
-				"al583060");
+				"root","al583060");
 		
-		System.out.println("Cerrando la conexion");
+		System.out.println("Cerrando la conexion, gracias");
 		
 		con.close();
 	}
